9882d5ab2dc78ea82c788a86398e20d09bde0c8e
Ibrahim <suleimanonnet@gmail.com>
2023-11-16 19:27:15 +0100

    Fix cors issues

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cors());
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
